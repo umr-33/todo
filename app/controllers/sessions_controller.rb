cd9a476ca97520eb5f30ea541798dfde569e5489
umr-33 <xx_yr@icloud.com>
2019-12-28 17:23:29 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(account_id: params[:session][:account_id])
+    if user&.authenticate(params[:session][:password])
+      login user
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'アカウントID、またはパスワードが間違っています。'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [] do
+    resources :tasks, only: :index
+  end
+  resources :tasks, only: :create
 end
